8eab212b8f4f06f7f3cea166e655c3ce95e035c4
Utkarsh Abhik <utkarshabhik1@gmail.com>
2026-02-27 18:03:12 +0530

    Create Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive. There is only one repeated number in nums, return this repeated number. You must solve the problem without modifying the array nums and using only constant extra space.py

---
+++ Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive. There is only one repeated number in nums, return this repeated number. You must solve the problem without modifying the array nums and using only constant extra space.py
@@ -0,0 +1,25 @@
+def find_duplicate(nums):
+    # Phase 1: Detect cycle
+    slow = nums[0]
+    fast = nums[0]
+    
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        
+        if slow == fast:
+            break
+    
+    # Phase 2: Find cycle start (duplicate)
+    slow = nums[0]
+    
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    
+    return slow
+
+
+# Example usage
+nums = [1, 3, 4, 2, 2]
+print(find_duplicate(nums))
